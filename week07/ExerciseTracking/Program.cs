cce3d323abb1ac1b13599238bd985e7e3ca94499
Kamder-4201 <jeff.bernard.candelario@gmail.com>
2025-10-16 01:07:34 +0800

    tested running.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,11 +5,15 @@ class Program
     // testing activity.cs
     static void Main(string[] args)
     {
-        // Create a sample Activity object
-        Activity test = new Activity(DateTime.Now, 30);
-        // Print the summary (will use placeholder values from base class)
-        Console.WriteLine(test.GetSummary());
-        Console.WriteLine();
+        // // Create a sample Activity object
+        // Activity test = new Activity(DateTime.Now, 30);
+        // // Print the summary (will use placeholder values from base class)
+        // Console.WriteLine(test.GetSummary());
+        // Console.WriteLine();
+
+        // Test the Running class
+        Running run = new Running(new DateTime(2025, 10, 13), 30, 3.0); // 3 miles in 30 min
+        Console.WriteLine(run.GetSummary());
     }
     
 }
